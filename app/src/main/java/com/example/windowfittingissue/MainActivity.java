69934a6576cb3607296e16b819d8fa2ff8772ff8
Christoph Wiesner <christoph.wiesner@runtastic.com>
2017-10-17 09:11:09 +0200

    Remove unnecessary setting of WTFs

--- app/src/main/java/com/example/windowfittingissue/MainActivity.java
+++ app/src/main/java/com/example/windowfittingissue/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.windowfittingissue;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.example.windowfittingissue.databinding.ActivityMainBinding;
 
@@ -14,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 		setSupportActionBar(binding.toolbar);
